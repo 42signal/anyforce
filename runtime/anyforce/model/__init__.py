e6dd7976b715c2441343e20295625dcf1b58fc13
exherb <i@4leaf.me>
2024-11-05 15:48:57 +0800

    feat(runtime): remove connections

--- runtime/anyforce/model/__init__.py
+++ runtime/anyforce/model/__init__.py
@@ -1,19 +1,16 @@
 from typing import Any, Dict
 
 from tortoise import Tortoise
-from tortoise.backends.base.client import BaseDBAsyncClient
 
 from .base import BaseModel, BaseUpdateModel
 from .enum import IntEnum, StrEnum
 from .recoverable import RecoverableModel
 
-connections: Dict[str, BaseDBAsyncClient] = {}
-
 
 async def init(config: Dict[str, Any]):
     await Tortoise.init(config=config)  # type: ignore
     for k in config["apps"]:
-        connections[k] = Tortoise.get_connection(k)
+        Tortoise.get_connection(k)
 
 
 def init_models(config: Dict[str, Any]):
