2d3f0b84169bcb3fcb51a3cc0171445d5eb8ed61
exherb <i@4leaf.me>
2025-03-12 16:04:51 +0800

    fix(runtime): logging

--- runtime/anyforce/logging/__init__.py
+++ runtime/anyforce/logging/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 from typing import Any
 
@@ -12,25 +13,35 @@ INFO = logging.INFO
 DEBUG = logging.DEBUG
 NOTSET = logging.NOTSET
 
+log_level = logging.getLevelNamesMapping().get(
+    os.environ.get("LOGLEVEL", "INFO").upper(), logging.INFO
+)
+
 shared_processors: list[structlog.typing.Processor] = [
     structlog.contextvars.merge_contextvars,
     structlog.processors.add_log_level,
+    structlog.processors.StackInfoRenderer(),
     structlog.processors.format_exc_info,
 ]
 if sys.stderr.isatty():
     processors = shared_processors + [structlog.dev.ConsoleRenderer()]
+    logger_factory = structlog.WriteLoggerFactory()
 else:
     processors = shared_processors + [
         structlog.processors.dict_tracebacks,
         structlog.processors.JSONRenderer(serializer=orjson.dumps),
     ]
+    logger_factory = structlog.BytesLoggerFactory()
 
 structlog.configure(
     cache_logger_on_first_use=True,
+    wrapper_class=structlog.make_filtering_bound_logger(log_level),
     processors=processors,
-    logger_factory=structlog.BytesLoggerFactory(),
+    logger_factory=logger_factory,
 )
 
 
-def get_logger(*names: str, **initial_values: Any) -> structlog.stdlib.BoundLogger:
+def get_logger(
+    *names: str, **initial_values: Any
+) -> structlog.typing.FilteringBoundLogger:
     return structlog.get_logger(*names, **initial_values)
